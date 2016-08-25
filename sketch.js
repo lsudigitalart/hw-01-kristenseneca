95fa87f426cec93eb5491382111eb31d9f16f674
Kristen Seneca <kristenseneca@3c-15-c2-dd-c9-6.wlan.lsu.edu>
2016-08-25 11:39:02 -0500

    trying it out

---
+++ sketch.js
@@ -0,0 +1,440 @@
+
+function setup(){
+  createCanvas(1450, 800);
+
+  fill(0, 0, 0)
+  noStroke();
+  ellipse(0, 200, 400);
+
+  fill(255, 255, 255)
+  noStroke();
+  ellipse(50, 200, 400);
+
+  fill(0, 0, 0)
+  noStroke();
+  ellipse(100, 200, 400);
+
+  fill(255, 255, 255)
+  noStroke();
+  ellipse(150, 200, 400);
+
+  fill(0, 0, 0)
+  noStroke();
+  ellipse(200, 200, 400);
+
+  fill(255, 255, 255)
+  noStroke();
+  ellipse(250, 200, 400);
+
+  fill(0, 0, 0)
+  noStroke();
+  ellipse(300, 200, 400);
+
+  fill(255, 255, 255)
+  noStroke();
+  ellipse(350, 200, 400);
+
+  fill(0, 0, 0)
+  noStroke();
+  ellipse(400, 200, 400);
+
+  fill(255, 255, 255)
+  noStroke();
+  ellipse(450, 200, 400);
+
+  fill(0, 0, 0)
+  noStroke();
+  ellipse(500, 200, 400);
+
+  fill(255, 255, 255)
+  noStroke();
+  ellipse(550, 200, 400);
+
+  fill(0, 0, 0)
+  noStroke();
+  ellipse(600, 200, 400);
+
+  fill(255, 255, 255)
+  noStroke();
+  ellipse(650, 200, 400);
+
+  fill(0, 0, 0)
+  noStroke();
+  ellipse(700, 200, 400);
+
+  fill(255, 255, 255)
+  noStroke();
+  ellipse(750, 200, 400);
+
+  fill(0, 0, 0)
+  noStroke();
+  ellipse(800, 200, 400);
+
+  fill(255, 255, 255)
+  noStroke();
+  ellipse(850, 200, 400);
+
+  fill(0, 0, 0)
+  noStroke();
+  ellipse(900, 200, 400);
+
+  fill(255, 255, 255)
+  noStroke();
+  ellipse(950, 200, 400);
+
+  fill(0, 0, 0)
+  noStroke();
+  ellipse(1000, 200, 400);
+
+  fill(255, 255, 255)
+  noStroke();
+  ellipse(1050, 200, 400);
+
+  fill(0, 0, 0)
+  noStroke();
+  ellipse(1100, 200, 400);
+
+  fill(255, 255, 255)
+  noStroke();
+  ellipse(1150, 200, 400);
+
+  fill(0, 0, 0)
+  noStroke();
+  ellipse(1200, 200, 400);
+
+  fill(255, 255, 255)
+  noStroke();
+  ellipse(1250, 200, 400);
+
+  fill(0, 0, 0)
+  noStroke();
+  ellipse(1300, 200, 400);
+
+  fill(255, 255, 255)
+  noStroke();
+  ellipse(1350, 200, 400);
+
+  fill(0, 0, 0)
+  noStroke();
+  ellipse(1400, 200, 400);
+
+  fill(255, 255, 255)
+  noStroke();
+  ellipse(1450, 200, 400);
+
+  fill(0, 0, 0)
+  noStroke();
+  ellipse(1500, 200, 400);
+
+  fill(255, 255, 255)
+  noStroke();
+  ellipse(1550, 200, 400);
+
+  fill(0, 0, 0)
+  noStroke();
+  ellipse(1600, 200, 400);
+
+  fill(255, 255, 255)
+  noStroke();
+  ellipse(1650, 200, 400);
+
+  fill(0, 0, 0)
+  noStroke();
+  ellipse(1700, 200, 400);
+
+  fill(255, 255, 255)
+  noStroke();
+  ellipse(1750, 200, 400);
+
+  fill(0, 0, 0)
+  noStroke();
+  ellipse(0, 595, 400);
+
+  fill(255, 255, 255)
+  noStroke();
+  ellipse(50, 595, 400);
+
+
+
+    fill(0, 0, 0)
+    noStroke();
+    ellipse(100, 595, 400);
+
+    fill(255, 255, 255)
+    noStroke();
+    ellipse(150, 595, 400);
+
+    fill(0, 0, 0)
+    noStroke();
+    ellipse(200, 595, 400);
+
+    fill(255, 255, 255)
+    noStroke();
+    ellipse(250, 595, 400);
+
+    fill(0, 0, 0)
+    noStroke();
+    ellipse(300, 595, 400);
+
+    fill(255, 255, 255)
+    noStroke();
+    ellipse(350, 595, 400);
+
+    fill(0, 0, 0)
+    noStroke();
+    ellipse(400, 595, 400);
+
+    fill(255, 255, 255)
+    noStroke();
+    ellipse(450, 595, 400);
+
+    fill(0, 0, 0)
+    noStroke();
+    ellipse(500, 595, 400);
+
+    fill(255, 255, 255)
+    noStroke();
+    ellipse(550, 595, 400);
+
+    fill(0, 0, 0)
+    noStroke();
+    ellipse(600, 595, 400);
+
+    fill(255, 255, 255)
+    noStroke();
+    ellipse(650, 595, 400);
+
+    fill(0, 0, 0)
+    noStroke();
+    ellipse(700, 595, 400);
+
+    fill(255, 255, 255)
+    noStroke();
+    ellipse(750, 595, 400);
+
+    fill(0, 0, 0)
+    noStroke();
+    ellipse(800, 595, 400);
+
+    fill(255, 255, 255)
+    noStroke();
+    ellipse(850, 595, 400);
+
+    fill(0, 0, 0)
+    noStroke();
+    ellipse(900, 595, 400);
+
+    fill(255, 255, 255)
+    noStroke();
+    ellipse(950, 595, 400);
+
+    fill(0, 0, 0)
+    noStroke();
+    ellipse(1000, 595, 400);
+
+    fill(255, 255, 255)
+    noStroke();
+    ellipse(1050, 595, 400);
+
+    fill(0, 0, 0)
+    noStroke();
+    ellipse(1100, 595, 400);
+
+    fill(255, 255, 255)
+    noStroke();
+    ellipse(1150, 595, 400);
+
+    fill(0, 0, 0)
+    noStroke();
+    ellipse(1200, 595, 400);
+
+    fill(255, 255, 255)
+    noStroke();
+    ellipse(1250, 595, 400);
+
+    fill(0, 0, 0)
+    noStroke();
+    ellipse(1300, 595, 400);
+
+    fill(255, 255, 255)
+    noStroke();
+    ellipse(1350, 595, 400);
+
+    fill(0, 0, 0)
+    noStroke();
+    ellipse(1400, 595, 400);
+
+    fill(255, 255, 255)
+    noStroke();
+    ellipse(1450, 595, 400);
+
+    fill(0, 0, 0)
+    noStroke();
+    ellipse(1500, 595, 400);
+
+    fill(255, 255, 255)
+    noStroke();
+    ellipse(1550, 595, 400);
+
+    fill(0, 0, 0)
+    noStroke();
+    ellipse(1600, 595, 400);
+
+    fill(255, 255, 255)
+    noStroke();
+    ellipse(1650, 595, 400);
+
+    fill(0, 0, 0)
+    noStroke();
+    ellipse(1700, 595, 400);
+
+    fill(255, 255, 255)
+    noStroke();
+    ellipse(1750, 595, 400);
+
+
+
+      fill(0, 0, 0)
+      noStroke();
+      ellipse(0, 397.5, 400);
+
+      fill(255, 255, 255)
+      noStroke();
+      ellipse(50, 397.5, 400);
+
+      fill(0, 0, 0)
+      noStroke();
+      ellipse(100, 397.5, 400);
+
+      fill(255, 255, 255)
+      noStroke();
+      ellipse(150, 397.5, 400);
+
+      fill(0, 0, 0)
+      noStroke();
+      ellipse(200, 397.5, 400);
+
+      fill(255, 255, 255)
+      noStroke();
+      ellipse(250, 397.5, 400);
+
+      fill(0, 0, 0)
+      noStroke();
+      ellipse(300, 397.5, 400);
+
+      fill(255, 255, 255)
+      noStroke();
+      ellipse(350, 397.5, 400);
+
+      fill(0, 0, 0)
+      noStroke();
+      ellipse(400, 397.5, 400);
+
+      fill(255, 255, 255)
+      noStroke();
+      ellipse(450, 397.5, 400);
+
+      fill(0, 0, 0)
+      noStroke();
+      ellipse(500, 397.5, 400);
+
+      fill(255, 255, 255)
+      noStroke();
+      ellipse(550, 397.5, 400);
+
+      fill(0, 0, 0)
+      noStroke();
+      ellipse(600, 397.5, 400);
+
+      fill(255, 255, 255)
+      noStroke();
+      ellipse(650, 397.5, 400);
+
+      fill(0, 0, 0)
+      noStroke();
+      ellipse(700, 397.5, 400);
+
+      fill(255, 255, 255)
+      noStroke();
+      ellipse(750, 397.5, 400);
+
+      fill(0, 0, 0)
+      noStroke();
+      ellipse(800, 397.5, 400);
+
+      fill(255, 255, 255)
+      noStroke();
+      ellipse(850, 397.5, 400);
+
+      fill(0, 0, 0)
+      noStroke();
+      ellipse(900, 397.5, 400);
+
+      fill(255, 255, 255)
+      noStroke();
+      ellipse(950, 397.5, 400);
+
+      fill(0, 0, 0)
+      noStroke();
+      ellipse(1000, 397.5, 400);
+
+      fill(255, 255, 255)
+      noStroke();
+      ellipse(1050, 397.5, 400);
+
+      fill(0, 0, 0)
+      noStroke();
+      ellipse(1100, 397.5, 400);
+
+      fill(255, 255, 255)
+      noStroke();
+      ellipse(1150, 397.5, 400);
+
+      fill(0, 0, 0)
+      noStroke();
+      ellipse(1200, 397.5, 400);
+
+      fill(255, 255, 255)
+      noStroke();
+      ellipse(1250, 397.5, 400);
+
+      fill(0, 0, 0)
+      noStroke();
+      ellipse(1300, 397.5, 400);
+
+      fill(255, 255, 255)
+      noStroke();
+      ellipse(1350, 397.5, 400);
+
+      fill(0, 0, 0)
+      noStroke();
+      ellipse(1400, 397.5, 400);
+
+      fill(255, 255, 255)
+      noStroke();
+      ellipse(1450, 397.5, 400);
+
+      fill(0, 0, 0)
+      noStroke();
+      ellipse(1500, 397.5, 400);
+
+      fill(255, 255, 255)
+      noStroke();
+      ellipse(1550, 397.5, 400);
+
+      fill(0, 0, 0)
+      noStroke();
+      ellipse(1600, 397.5, 400);
+
+      fill(255, 255, 255)
+      noStroke();
+      ellipse(1650, 397.5, 400);
+
+      fill(0, 0, 0)
+      noStroke();
+      ellipse(1700, 397.5, 400);
+
+      fill(255, 255, 255)
+      noStroke();
+      ellipse(1750, 397.5, 400);
+}
